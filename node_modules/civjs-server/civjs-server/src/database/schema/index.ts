bf4e320f2794f1f77d1f981a399debd22374db0e
Michael Chu <mchchu88@gmail.com>
2025-08-22 22:19:57 -0400

    Remove node_modules from version control and add .gitignore
    - Removed entire node_modules directory from git tracking
    - Added comprehensive .gitignore file for Node.js projects
    - This prevents node_modules from being committed in the future
    - Dependencies can be recreated with npm/yarn install

--- node_modules/civjs-server/civjs-server/src/database/schema/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all schema tables and types
-export * from './users';
-export * from './games';
-export * from './players';
-export * from './cities';
-export * from './units';
-export * from './game-turns';
-export * from './research';
